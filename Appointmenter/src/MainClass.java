1b0c683e895eccd34840ca86aa210833bf12981d
hacksonx <mbhiza.shaun@gmail.com>
2016-08-19 13:17:41 +0200

    Edits by Shaun for testing.

--- Appointmenter/src/MainClass.java
+++ Appointmenter/src/MainClass.java
@@ -4,10 +4,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println();
+		System.out.println("AppointMenter");
 		
 		//
-		System.out.println();
+		System.out.println("Made by Shaun");
 
 	}
 
